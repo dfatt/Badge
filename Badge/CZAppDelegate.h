fa13b9a7faefa9c6a58baaa297e57ccc0639f3cf
William Adama <damir.fattahov@gmail.com>
2012-04-10 14:58:14 +0600

    Initial Commit

---
+++ Badge/CZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CZAppDelegate.h
+//  Badge
+//
+//  Created by Дамир Фаттахов on 10.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CZAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Badge/CZAppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  CZAppDelegate.m
+//  Badge
+//
+//  Created by Дамир Фаттахов on 10.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CZAppDelegate.h"
+
+@implementation CZAppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
